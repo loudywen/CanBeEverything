66b1a6ed0c73871242e7acec122b3b9a0ce03f3a
Devon <loudywen@gmail.com>
2016-09-07 12:09:38 -0500

    first commit

---
+++ CanBeEverything/src/main/java/com/devon/feelgood/CanBeEverythingApplication.java
@@ -0,0 +1,12 @@
+package com.devon.feelgood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanBeEverythingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanBeEverythingApplication.class, args);
+	}
+}
---
+++ CanBeEverything/src/test/java/com/devon/feelgood/CanBeEverythingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.devon.feelgood;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CanBeEverythingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
